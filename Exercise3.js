6c1fbc1f7936051ecfc746c4d53523b682ffa7ca
jennieines <141196314+jennieines@users.noreply.github.com>
2023-08-06 19:51:07 -0400

    Create Exercise3.js

---
+++ Exercise3.js
@@ -0,0 +1,10 @@
+console.log("This is a string, it has quotes.");
+console.log(100);
+console.log("100");
+
+console.log("My name is: ");
+console.log('Jennie'); //change this line
+console.log("My age is: ");
+console.log('27'); //change this line, which type should you use?
+console.log("My zip code is: "); 
+console.log('33778'); //change this line, which type should you use?
