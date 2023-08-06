37d15c5794c96ada322becb3d1f38866b8df3d4f
jennieines <141196314+jennieines@users.noreply.github.com>
2023-08-06 16:43:38 -0400

    Create Exercise1.js

---
+++ Exercise1.js
@@ -0,0 +1,5 @@
+console.log("There once was a writer of code");
+console.log('Who wanted to learn JS and node');
+console.log('They said "Once I can learn,"');
+console.log("'I'll increase what I earn.'");
+console.log("So now they're in full programmer mode!");
