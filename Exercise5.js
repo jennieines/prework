aff579edfc13b09160352133fa1b9bc92edc0c02
jennieines <141196314+jennieines@users.noreply.github.com>
2023-08-10 17:47:12 -0400

    Create Exercise5.js

---
+++ Exercise5.js
@@ -0,0 +1,12 @@
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+
+//Part.2
+console.log(2 == 2); // ==, true
+console.log(2 > 5); // >, false
+console.log(6 <= 4); // <=, false
+console.log(20 != 14); // !=, true
